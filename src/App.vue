2c01f903dc4e8f9f032149311b1347442a9eb7c3
Paulina Karadagian <78479614+PaulinaKaradagian@users.noreply.github.com>
2021-12-31 18:56:37 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    
+    <weatherApp/>
+  </div>
+</template>
+<script>
+import weatherApp from './components/weather-app.vue'
+export default {
+ name: 'App',
+ components: {
+  weatherApp,
+ }
+}
+</script>
